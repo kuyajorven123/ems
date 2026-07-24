fbbd85b0123dff3da4c15d4a1d129405c69c1a49
kuyajorven123 <kuyajorven@gmail.com>
2026-07-24 22:23:01 +0800

    --

--- src/main/java/com/project/ems/controller/LeaveController.java
+++ src/main/java/com/project/ems/controller/LeaveController.java
@@ -1,16 +1,34 @@
 package com.project.ems.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 import org.springframework.ui.Model;
+import com.project.ems.model.LeaveRepository;
+import com.project.ems.model.UserRepository;
+import com.project.ems.model.User;
 
 @Controller
 public class LeaveController {
 
+    @Autowired
+    private LeaveRepository leaveRepository;
+    private UserRepository userRepository;
+
+    public LeaveController(LeaveRepository leaveRepository, UserRepository userRepository) {
+        this.leaveRepository = leaveRepository;
+        this.userRepository = userRepository;
+    }
+
     @GetMapping({ "/apply_leave" })
-    public String apply_leave(Model model) {
+    public String apply_leave(Model model, Authentication authentication) {
+
+        User user = userRepository.findByEmail(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("User not Found"));
         model.addAttribute("activePage", "apply_leave");
+        model.addAttribute("leave", leaveRepository.findByUserId(user));
         return "/pages/apply_leave";
     }
 
---
+++ src/main/java/com/project/ems/model/Leave.java
@@ -0,0 +1,93 @@
+package com.project.ems.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "leaves")
+public class Leave {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "leave_type")
+    private String leaveType;
+    @Column(name = "start_date")
+    private LocalDate startDate;
+    @Column(name = "end_date")
+    private LocalDate endDate;
+    @Column(name = "date_applied")
+    private LocalDate dateApplied;
+    private String reason;
+    private String status;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getDateApplied() {
+        return dateApplied;
+    }
+
+    public void setDateApplied(LocalDate dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+}
